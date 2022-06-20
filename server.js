623db7d66097634d0159d79f9037c2d116da4561
Ben <byeungis@gmail.com>
2022-06-20 12:35:41 -0400

    chap 2

--- server.js
+++ server.js
@@ -10,6 +10,15 @@ const PORT = process.env.PORT || 3001;
 //instantiate the server " make it listen"
 const app = express();
 
+const fs = require("fs");
+//library that works with file and directory paths
+const path = require("path");
+
+//parse incoming string or array data this and the other use with express.json is what will display on insomnia
+app.use(express.urlencoded({ extended: true }));
+//parse incoming JSON data. this used with the above urlencoded to display on insomnia
+app.use(express.json());
+
 //function will take in re.query as an argument and filter through animals accordingly, returning new filter array
 function filterByQuery(query, animalsArray) {
   let personalityTraitsArray = [];
@@ -62,6 +71,18 @@ function findById(id, animalsArray) {
   return result;
 }
 
+//accepts the post route's value, retrieves information from entry and then repost it
+//using fs.writefilesync funciton method to synchronous version of fs.writefile and doesnt requrie callback, we want our animals.json file in the data subdirectory, so usting method path.jooin() to join value of __direname"which represents tthe directory of the file we excuted the code in with the animals.json path.  need too save JAVASCRIPT array data as JSON, so we stringify it.  null means we dont want to edite any of our existing data, if we did, we could pass somethgin in there.  2 dinicates we want to create white space betwen our values to make it more readable.
+function createNewAnimal(body, animalsArray) {
+  const animal = body;
+  animalsArray.push(animal);
+  fs.writeFileSync(
+    path.join(__dirname, "./data/animals.json"),
+    JSON.stringify({ animals: animalsArray }, null, 2)
+  );
+  return animal;
+}
+
 //adding route for animals, get() method requires 2 arguments, string that describes the route the client will fetch, and the second ais the call back.
 app.get("/api/animals", (req, res) => {
   let results = animals;
@@ -84,6 +105,39 @@ app.get("/api/animals/:id", (req, res) => {
     res.send(404);
   }
 });
+
+app.post("/api/animals", (req, res) => {
+  //req.body is where our incoming content will be
+  //set id based on what the next index of the array will e, sends out the number on the ID
+  req.body.id = animals.length.toString();
+
+  //if any data in req.body is incorrect, send 400 error back
+  if (!validatedAnimal(req.body)) {
+    res.status(400).send("The animal is not properly formated.");
+  } else {
+    //add animal to json file and animals array in this function
+    const animal = createNewAnimal(req.body, animals);
+
+    res.json(animal);
+  }
+});
+
+function validatedAnimal(animal) {
+  if (!animal.name || typeof animal.name !== "string") {
+    return false;
+  }
+  if (!animal.species || typeof animal.species !== "string") {
+    return false;
+  }
+  if (!animal.diet || typeof animal.diet !== "string") {
+    return false;
+  }
+  if (!animal.personalityTraits || !Array.isArray(animal.personalityTraits)) {
+    return false;
+  }
+  return true;
+}
+
 //chaining listen method to server and port, for example 82.458.45863.5633 that whole things
 app.listen(PORT, () => {
   console.log(`API server now on port ${PORT}!`);
