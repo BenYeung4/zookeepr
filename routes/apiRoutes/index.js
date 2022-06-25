e8acf3a40f2deac04e9c14577187444b7b0cce8d
Ben <byeungis@gmail.com>
2022-06-24 22:10:15 -0400

    update zookeeper

--- routes/apiRoutes/index.js
+++ routes/apiRoutes/index.js
@@ -1,7 +1,9 @@
 //this file is to employing the Router as the other files, we are as module exported from animalRoutes.js, we are using both apiRoutes/index.js as centralhub for all routing function
 const router = require("express").Router();
 const animalRoutes = require("../apiRoutes/animalRoutes");
-
 router.use(animalRoutes);
+
+//need this to run the zookeeperRoutes.js, could use this version or the animalRoutes above
+router.use(require("./zookeeperRoutes"));
 
 module.exports = router;
